618986f55d1990747036c59732f8e58e484ac0e7
pranav123321 <98205408+pranav123321@users.noreply.github.com>
2022-02-05 18:25:45 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas;
+var music;
+createSprite(random(20,750))
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    //create 4 different surfaces
+
+
+
+    //create box sprite and give velocity
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    //create edgeSprite
+
+if(surface1.isTouching(box) && box.bounceOff(surface1)){}
+
+    //add condition to check if box touching surface and make it box
+}
